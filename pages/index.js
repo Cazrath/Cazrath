33f48affbd60e518d7244f70eaff502303e8fd94
Cazrath <kakarot75@yahoo.com>
2025-07-04 08:16:26 +0530

    index.js

--- pages/index.js
+++ pages/index.js
@@ -91,3 +91,23 @@ export default function Home() {
     </div>
   );
 }
+import { useEffect } from 'react';
+import '../styles/globals.css'; // This can stay or be removed based on your setup
+
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const clearAccess = () => {
+      localStorage.removeItem('hasAccess');
+    };
+
+    window.addEventListener('beforeunload', clearAccess);
+
+    return () => {
+      window.removeEventListener('beforeunload', clearAccess);
+    };
+  }, []);
+
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
