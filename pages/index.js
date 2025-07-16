b86cd7a62a53b29a9e947eec455b3c237b054ce1
Cazrath <kakarot75@yahoo.com>
2025-07-16 08:27:55 +0530

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div style={{ color: 'white', backgroundColor: 'black', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
+      <h1>Cazrath</h1>
+      <p>even if the sky falls, the soul it feared most — Cazrath — will still be standing where gravity ends.</p>
+    </div>
+  );
+}
