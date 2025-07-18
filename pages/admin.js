4af41263750a1c673565e46e571af9f882157cba
Cazrath <kakarot75@yahoo.com>
2025-07-18 08:07:30 +0530

    admin.js

---
+++ pages/admin.js
@@ -0,0 +1,10 @@
+import AdminTools from '../components/AdminTools'
+
+export default function Admin() {
+  return (
+    <div className="admin-container">
+      <h2>CAZRATH DASHBOARD</h2>
+      <AdminTools />
+    </div>
+  )
+}
