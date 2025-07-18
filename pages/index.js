4a8961bb21ab4b78fb04038a1033b79a21094361
Cazrath <kakarot75@yahoo.com>
2025-07-18 08:06:51 +0530

    index.js

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import CLogo from '../components/CLogo'
+import Gallery from '../components/Gallery'
+
+export default function Home() {
+  return (
+    <div className="cazrath-container">
+      <CLogo />
+      <h1>ℭ𝔞𝔷𝔯𝔞𝔱𝔥</h1>
+      <Gallery />
+    </div>
+  )
+}
