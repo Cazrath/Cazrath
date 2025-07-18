50830e699c17d53a31d08cbd8c786d1e70b81951
Cazrath <kakarot75@yahoo.com>
2025-07-18 08:05:21 +0530

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: "2rem", fontFamily: "monospace" }}>
-      <h1>Welcome to Cazrath</h1>
-      <p>Visit /fragments to view the image gallery.</p>
-    </div>
-  );
-}
