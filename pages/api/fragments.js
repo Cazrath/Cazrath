120e29785b55f848c095756c0564d6ac000596ca
Cazrath <kakarot75@yahoo.com>
2025-07-20 14:11:16 +0530

    Delete pages directory

--- pages/api/fragments.js
+++
@@ -1,6 +0,0 @@
-import { prisma } from '../../lib/db'
-
-export default async function handler(req, res) {
-  const fragments = await prisma.fragment.findMany()
-  res.status(200).json(fragments)
-}
