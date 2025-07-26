9ba4af0b38265c0b8ecdca16e8e906f45d2c1a83
Cazrath <kakarot75@yahoo.com>
2025-07-27 01:51:42 +0530

    Add files via upload

---
+++ movies.js
@@ -0,0 +1,19 @@
+
+export const movies = [
+  {
+    id: "caz01",
+    title: "Night of the Living Dead",
+    year: "1968",
+    description: "A group of people try to survive an attack of bloodthirsty zombies.",
+    video: "/movies/night_of_the_living_dead.mp4",
+    thumbnail: "/movies/night_thumbnail.jpg"
+  },
+  {
+    id: "caz02",
+    title: "Plan 9 from Outer Space",
+    year: "1959",
+    description: "Aliens resurrect dead humans as part of a plan to stop humanity from creating a doomsday weapon.",
+    video: "/movies/plan9.mp4",
+    thumbnail: "/movies/plan9_thumbnail.jpg"
+  }
+];
